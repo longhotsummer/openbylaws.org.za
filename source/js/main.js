986fca984e39ec099ff8572968ec1098f0fa2911
Greg Kempe <greg@kempe.net>
2016-02-21 17:56:44 +0200

    Simplify homepage

--- source/js/main.js
+++ source/js/main.js
@@ -17,31 +17,6 @@ $(function() {
       return $('#defn-' + term_id).html();
     }
   });
-
-  // load recent blog posts
-  (function() {
-    var container = $('#recent-blog-posts');
-
-    if (container.length && tumblr_api_read) {
-      var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-      $.each(tumblr_api_read.posts, function(i, post) {
-        var li = $('#recent-blog-posts li.template').clone();
-        var date = new Date(post['unix-timestamp']*1000);
-
-        $('.day', li).text(date.getDate());
-        $('.month', li).text(months[date.getMonth()]);
-
-        $('h4', li).text(post['regular-title']);
-        $('p', li).text(post['regular-body'].slice(0, 200) + "...");
-        $('a', li).attr('href', post['url-with-slug']);
-
-        li.removeClass('hidden');
-        li.removeClass('template');
-        container.append(li);
-      });
-    }
-  })();
 });
 
 $(function() {
