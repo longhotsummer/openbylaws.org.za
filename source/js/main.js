d9426c3c58f12e7db916192b290a196973ae9d80
Greg Kempe <greg@kempe.net>
2017-04-05 10:14:27 +0200

    Add zopim

--- source/js/main.js
+++ source/js/main.js
@@ -74,3 +74,10 @@ $(function() {
     });
   }
 });
+
+// Start of Zendesk Chat Script
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?4i7Iq4DZ2A1k3ggcnEZBWXw7YB8IZsnG";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
